d85617e69bb7b7a9af691a1d713b68eff319d955
Xibalsba <jl573160@gmail.com>
2022-07-26 16:58:15 -0500

    [add] Diseño del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
+import MyWalletIcon from '../assets/images/my-wallet.png';
 
 export const Navbar = () => {
-  return (
-    <>
-        <h2>Navbar</h2>
-    </>
+  return ( 
+    <nav className="bg-white border-gray-300 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <div className="flex items-center">
+            <img src={ MyWalletIcon } className="mr-3 h-6 sm:h-9" alt="My Wallet Icon" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">My Wallet</span>
+        </div>
+
+        <div class="flex items-center">
+            <span className="mr-6 text-lg font-medium text-green-500 dark:text-white">$1,200.00</span>
+        </div>
+      </div>
+    </nav>
   )
 }
