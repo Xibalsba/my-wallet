a68547b74a551ba6c713b0a25849c4318914966f
Xibalsba <jl573160@gmail.com>
2022-07-26 15:55:23 -0500

    [add] Archivo de navbar

--- src/MyWallet.jsx
+++ src/MyWallet.jsx
@@ -1,8 +1,9 @@
+import { Navbar } from "./components/Navbar"
 
 export const MyWallet = () => {
     return (
         <>
-            <h1 className="text-3xl">My Wallet</h1>
+            <Navbar />
         </>
     )
 }
